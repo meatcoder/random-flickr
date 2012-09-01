106f05f7e85273845f9c6e45dad0c4c53cbe7af2
Sim Domingo <sim@purkee.com>
2012-09-01 14:06:35 +0800

    Made flicker user id and tag environment variables for more flexibility.

--- web.rb
+++ web.rb
@@ -5,8 +5,8 @@ def random_pic_url
   FlickRaw.api_key = ENV['FLICKR_API_KEY']
   FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET']
   
-  user_id = '86301377@N08'
-  list = flickr.photos.search :user_id => user_id, :tags => 'maomesvc'
+  user_id = ENV['FLICKR_USER_ID']
+  list = flickr.photos.search :user_id => user_id, :tags => ENV['FLICKR_TAG']
   ridx = rand(list.count)
   info = flickr.photos.getInfo :photo_id => list[ridx].id, :secret => list[ridx].secret
   
