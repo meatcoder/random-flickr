ec8f99a13003573dc31a9ba2bda92efb7b104129
Sim Domingo <sim@purkee.com>
2012-09-07 16:41:59 +0800

    Added test structure.

---
+++ Rakefile
@@ -0,0 +1,3 @@
+task :default do 
+  require 'test'
+end
---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'web'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class MyAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_home
+    get '/'
+    assert_equal 'Meow!', last_response.body
+  end
+end
